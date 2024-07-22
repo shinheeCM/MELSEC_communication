addfc12903bbbf862c286102f063742bcfc1633c
Dj <debanikroy92@gmail.com>
2024-07-22 15:53:47 +0900

    if d_2014_val == 2:

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def plc_communication():
     while True:
         # d_2010_val, dword_values = pymc3e.randomread(word_devices=["D2010"], dword_devices=[])
         d_2010_val = reading_resister_plc("D2010")
-        print("D2010 --> ", d_2010_val)
+        # print("D2010 --> ", d_2010_val)
         if write_data == 1:
             write_resister_plc("D2000", write_data)
             write_data = 0
@@ -30,20 +30,80 @@ def plc_communication():
             write_resister_plc("D2000", write_data)
             write_data = 1
         time.sleep(3)
-        
+
+def plc_communication1():
+    while True:
+        d_2011_val = reading_resister_plc("D2011")
+        d_2005_val = reading_resister_plc("D2005")
+        d_2012_val = reading_resister_plc("D2012")
+        d_2014_val = reading_resister_plc("D2014")
+        d_2014_val = reading_resister_plc("D2014")
+        print("d_2014_val ==> ", d_2014_val)
+        print(d_2011_val, d_2005_val, d_2012_val, d_2014_val)
+
+        if d_2014_val == 2:
+            print("d_2014_val == ")
+            write_resister_plc("D2001", 0)
+            break
+            # time.sleep(3)
+
+        if(d_2011_val and not d_2005_val and d_2012_val and not d_2014_val):
+            print("found ----> ")
+            write_resister_plc("D2003", 1)
+            time.sleep(3)
+            write_resister_plc("D2001", 1)
+            time.sleep(1)
+            write_resister_plc("D2003", 0)
+            # time.sleep(3)
+            # write_resister_plc("D2014", 1)
+            if d_2014_val == 1:
+                write_resister_plc("D2006", 1)
+
+            
+
+            # break
+
+
+# def plc_communication2():
+#     while True:
+#         d_2011_val = reading_resister_plc("D2011")
+#         d_2005_val = reading_resister_plc("D2005")
+#         d_2012_val = reading_resister_plc("D2012")
+#         d_2014_val = reading_resister_plc("D2014")
+
+#         print(d_2011_val, d_2005_val, d_2012_val, d_2014_val)
+#         if(d_2011_val and not d_2005_val and d_2012_val and not d_2014_val):
+#             print("found ----> ")
+#             write_resister_plc("D2003", 1)
+#             time.sleep(3)
+#             write_resister_plc("D2001", 1)
+#             time.sleep(3)
+#             write_resister_plc("D2003", 0)
+#             # time.sleep(3)
+#             write_resister_plc("D2014", 1)
+#             time.sleep(3)
+#             write_resister_plc("D2006", 1)
+#             # time.sleep(3)
+#             write_resister_plc("D2014", 2)
+#             time.sleep(3)
+#             write_resister_plc("D2001", 0)
+#             time.sleep(3)
+
 
 
 # Create a thread for the PLC communication
 plc_thread = threading.Thread(target=plc_communication)
-
 # Start the thread
 plc_thread.start()
 
+# plc_communication1()
+plc_thread1 = threading.Thread(target=plc_communication1)
+# Start the thread
+plc_thread1.start()
 # You can do other tasks in the main thread
-while True:
-    d_2011_val = reading_resister_plc("D2011")
-    d_2005_val = reading_resister_plc("D2005")
-    print("D2011 --> ", d_2011_val, "D2005 --> ", d_2005_val)
+
+        # print("")
+    # print("D2011 --> ", d_2011_val, "D2005 --> ", d_2005_val)
 
     # Main thread tasks
-    time.sleep(1)
+    
